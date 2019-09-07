991369451ac87863b10c5e142b9a9ffbcaa84d1d
Ahmad Akilan <ahmed.akilan@roamworks.com>
2019-09-07 08:31:08 +0400

    Added Comments

--- mqtt-subject.py
+++ mqtt-subject.py
@@ -7,19 +7,27 @@ from rx.subject import Subject
 from os import environ
 import paho.mqtt.client as mqtt
 
-class MqttSubject(Subject):
-    def __init__(self, topic, name=''):
+class EchoSubject(Subject):
+    def __init__(self, name=''):
+        # Run the super class constructor
         super().__init__()
-        self.topic = topic
-        self.client = mqtt.Client(name)
+        # Make topic property
+        self.topic = 'echo'
+        # Make an MQTT client
+        self.client = mqtt.Client('EchoClient')
+        # Pass subscribe as a callback to on_connect
         self.client.on_connect = lambda client, userdat, flags, rc: client.subscribe(self.topic, qos=1)
+        # Add the subject on_next as the callback of mqtt on_message and pass to it the payload
         self.client.message_callback_add(topic, lambda client, userdata, msg: self.on_next(msg.payload.decode("utf-8")))
+        # Establish connection
         self.client.connect(environ.get("MQTT_SERVICE","localhost"))
+        # Start the client
         self.client.loop_start()
 
-    # Ideally the subject is used eiether for sending or receiving.
-    # This send method is just for sample simplicity.
     def send(self, msg):
+        """
+        Publishes message to the MQTT topic
+        """
         print("sending msg: {}".format(msg))
         self.client.publish(self.topic, msg, qos=0, retain=False)
 
@@ -28,8 +36,9 @@ class MqttSubject(Subject):
         self.client.loop_stop()
         self.client.disconnect()
 
-# name the topic "echo" cause that it is what this sample doing
-s = MqttSubject(topic="echo")
+# Create echo client
+s = EchoSubject()
+# Subscribe print to the subject
 s.subscribe(on_next=lambda msg: print("received msg: {}".format(msg)))
 
 
--- rx-mqtt-ping-pong.py
+++ rx-mqtt-ping-pong.py
@@ -8,15 +8,22 @@ from os import environ
 import paho.mqtt.client as mqtt
 from time import sleep
 
-class MqttSubject(Subject):
+class PingPongSubject(Subject):
     def __init__(self, name, opponent_name):
+        # Run the super class constructor
         super().__init__()
+        # Set player and opponent names, which will servce as topics
         self.name = name
         self.opponent_name = opponent_name
+        # Set this player's topic. This is where he will receive
         self.client = mqtt.Client(name)
+        # Pass subscribe as a callback to on_connect
         self.client.on_connect = lambda client, userdat, flags, rc: client.subscribe(self.name, qos=1)
+        # Add callback receiver which will return PING or PONG and send the returned value to the opponent who will do the same
         self.client.message_callback_add(self.name, lambda client, userdata, msg: self.send(self.__receive_reply__(msg.payload.decode("utf-8"))))
+        # Establish connection
         self.client.connect(environ.get("MQTT_SERVICE", "localhost"))
+        # Start the client
         self.client.loop_start()
 
     def __receive_reply__(self, msg):
@@ -28,6 +35,9 @@ class MqttSubject(Subject):
             return "PING"
 
     def send(self, msg):
+        """
+        Publishes message (PING or PONG) to the opponenet topic
+        """
         print("player {} sending {}".format(self.name, msg))
         self.client.publish(self.opponent_name, msg, qos=0, retain=False)
 
@@ -36,15 +46,19 @@ class MqttSubject(Subject):
         self.client.loop_stop()
         self.client.disconnect()
 
-p1 = MqttSubject("player1", "player2")
-p2 = MqttSubject("player2", "player1")
+# Set up players
+p1 = PingPongSubject("player1", "player2")
+p2 = PingPongSubject("player2", "player1")
 
+# start the game
 p1.send(msg='PING')
 
 rx1 = p1.subscribe(on_next=lambda msg: print("p1 received: {}".format(msg)))
 rx2 = p2.subscribe(on_next=lambda msg: print("p2 received: {}".format(msg)))
 
+# Let them play for 10 seconds
 sleep(10)
 
+# times up
 p1.dispose()
 p2.dispose()
